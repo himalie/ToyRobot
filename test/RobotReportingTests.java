9b6537e52d3075fddbf4fc81cf6ce2d451ba0ba6
himalie <himalie1@gmail.com>
2016-02-28 18:06:40 +1100

    testing report with mocking

--- test/RobotReportingTests.java
+++ test/RobotReportingTests.java
@@ -1,8 +1,9 @@
 package test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import main.Board;
 import main.Direction;
 import main.Position;
 import main.PositionValidator;
@@ -16,20 +17,22 @@ public class RobotReportingTests {
 	private final Validatable _positionValidator;
 	
 	public RobotReportingTests(){
-		Board board = new Board(5,5);
-		_positionValidator = new PositionValidator(board);
+		_positionValidator = mock(PositionValidator.class);
 	}
 	
 	@Test
 	public void ShouldReportCurrentPositionIfPositioned() {
 		
+		Position position = new Position(1,2);		
+		
+		when(_positionValidator.validate(position)).thenReturn(true);
 		ToyRobot robot = new ToyRobot(_positionValidator);
+		robot.place(position, Direction.NORTH);
 		
-		Position defaultPosition = new Position(1,2);
-		robot.place(defaultPosition, Direction.NORTH);
-		String _output = robot.report();
+		when(robot.isPlaced()).thenReturn(true);
+		String output = robot.report();
 		
-		assertEquals("1, 2, NORTH", _output);
+		assertEquals("1, 2, NORTH", output);
 	}
 	@Test
 	public void ShouldNotReturnReportIfNotPlaced() {
