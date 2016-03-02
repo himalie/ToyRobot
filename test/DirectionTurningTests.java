f8a32336b7a9e3bcd497f3e3df42ecfce23f1100
himalie <himalie1@gmail.com>
2016-03-02 14:45:23 +1100

    testing turning left and right

--- test/DirectionTurningTests.java
+++ test/DirectionTurningTests.java
@@ -16,21 +16,23 @@ import org.junit.runners.Parameterized;
 @RunWith(Parameterized.class)
 public class DirectionTurningTests {
 
-	private Direction expectedDirection;
+	private Direction expectedDirectionTurnedLeft;
+	private Direction expectedDirectionTurnedRight;
 	private Direction inputDirection;	
 	
-	public DirectionTurningTests(Direction expectedDirection, Direction inputDirection) {
-		this.expectedDirection = expectedDirection;
+	public DirectionTurningTests(Direction expectedDirectionTurnedLeft, Direction expectedDirectionTurnedRight, Direction inputDirection) {
+		this.expectedDirectionTurnedLeft = expectedDirectionTurnedLeft;
+		this.expectedDirectionTurnedRight = expectedDirectionTurnedRight;
 		this.inputDirection = inputDirection;
 	}	
 	
 	@Parameterized.Parameters
-	   public static Collection leftTurnDirections() {
+	   public static Collection TurnDirections() {
 	      return Arrays.asList(new Object[][]{	    		 
-	 	         { Direction.EAST, Direction.SOUTH},
-	 	         { Direction.NORTH, Direction.EAST },
-	 	         { Direction.WEST, Direction.NORTH},
-	 	         { Direction.SOUTH, Direction.WEST }
+	 	         { Direction.EAST, Direction.WEST, Direction.SOUTH},
+	 	         { Direction.NORTH, Direction.SOUTH, Direction.EAST },
+	 	         { Direction.WEST, Direction.EAST, Direction.NORTH},
+	 	         { Direction.SOUTH, Direction.NORTH, Direction.WEST }
 	   });
 	}
 
@@ -40,7 +42,16 @@ public class DirectionTurningTests {
 		Turnable directionTurner= new DirectionTurner();
 		Direction _direction = directionTurner.Left(inputDirection);
 		
-		assertEquals(expectedDirection, _direction);
+		assertEquals(expectedDirectionTurnedLeft, _direction);
+		
+	}
+	@Test
+	public void ShouldTurnRightFromGivenDirection() {
+
+		Turnable directionTurner= new DirectionTurner();
+		Direction _direction = directionTurner.Right(inputDirection);
+		
+		assertEquals(expectedDirectionTurnedRight, _direction);
 		
 	}
 }
